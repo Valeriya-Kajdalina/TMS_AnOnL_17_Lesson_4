f56cbda2177c41a51e2b0c5b757fcdfec433a0ab
Valeriya Kajdalina <ValeriyaKajdalina@gmail.com>
2024-01-03 01:11:20 +0300

    add task_6

---
+++ lesson4/src/task6.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class task6 {
+    public static void main(String[] args) {
+        String[] array = {"Lera", "Sasha", "Kirill", "Andrew", "Tom"};
+        Arrays.sort(array);
+        System.out.println("Новый массив: "+ Arrays.toString(array));
+    }
+}
