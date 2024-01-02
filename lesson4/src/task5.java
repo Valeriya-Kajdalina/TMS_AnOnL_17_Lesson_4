add8678251a90d30724c431c2f9d1926abe2c295
Valeriya Kajdalina <ValeriyaKajdalina@gmail.com>
2024-01-02 22:01:32 +0300

    add task_5

---
+++ lesson4/src/task5.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class task5 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++)
+            array[i] = (int) (Math.random()*20);
+
+        System.out.println("Массив: "+ Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++)
+            if (i % 2 != 0)
+                array[i] = 0;
+
+        System.out.println("Новый массив: "+ Arrays.toString(array));
+    }
+}
